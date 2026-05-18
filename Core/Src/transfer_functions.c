7cfd3b94940073280cd36b35ece8779c134840e5
DrinkWaterRightNow <minhnguyen23721@gmail.com>
2026-05-18 04:56:25 +0300

    increased accuracy of acceleration pedal maps

--- Core/Src/transfer_functions.c
+++ Core/Src/transfer_functions.c
@@ -10,25 +10,21 @@
 #include "functions.h"
 #include "main.h"
 #include <math.h>
+static uint16_t bpps_global = 0;
 
-extern uint16_t v5_line_mv;
-
-uint32_t ValueControl(uint32_t raw, uint32_t min_raw, uint32_t max_raw)
+static inline uint32_t ValueControl(uint32_t raw, uint32_t min_raw, uint32_t max_raw)
 {
-
   if (raw > max_raw)
     return max_raw;
-
   if (raw < min_raw)
     return min_raw;
-
   return raw;
 }
 
 uint16_t TF_3V3(uint8_t bytes, uint32_t raw, Sensor* sensor)
 {
 
-  uint16_t max_volt = 3300;
+  const uint16_t max_volt = 3300;
 
   uint16_t voltage = raw * max_volt / 4095;
   return voltage;
@@ -36,7 +32,7 @@ uint16_t TF_3V3(uint8_t bytes, uint32_t raw, Sensor* sensor)
 
 uint16_t TF_5V(uint8_t bytes, uint32_t raw, Sensor* sensor)
 {
-  uint16_t max_volt = 5000;
+  const uint16_t max_volt = 5000;
 
   uint16_t voltage = raw * max_volt / 4095;
   return voltage;
@@ -50,10 +46,9 @@ uint16_t TF_24V(uint8_t bytes, uint32_t raw, Sensor* sensor)
   return voltage;
 }
 
-uint16_t bpps_global;
 uint16_t TF_BPPS(uint8_t bytes, uint32_t raw, Sensor* sensor)
-{                         // brake pedal position sensor
-  uint16_t max_pos = 100; // in percent
+{                               // brake pedal position sensor
+  const uint16_t max_pos = 100; // in percent
 
   uint16_t min_raw = (sensor->calib_code % 2 == 1 && sensor->low_adc != 0) ? sensor->low_adc : 2615;
   uint16_t max_raw = (sensor->calib_code > 2 && sensor->high_adc != 0) ? sensor->high_adc : 2925;
@@ -64,7 +59,6 @@ uint16_t TF_BPPS(uint8_t bytes, uint32_t raw, Sensor* sensor)
 
   if (min_raw > max_raw)
   {
-
     bpps = 100 - bpps;
     bpps_global = bpps;
   }
@@ -72,38 +66,38 @@ uint16_t TF_BPPS(uint8_t bytes, uint32_t raw, Sensor* sensor)
 }
 uint16_t TF_APPS1(uint8_t bytes, uint32_t raw, Sensor* sensor)
 {
-  uint16_t max_pos = 100; // in percent
+  const uint16_t max_pos = 1000; // in percent
 
   uint16_t min_raw = (sensor->calib_code % 2 == 1 && sensor->low_adc != 0) ? sensor->low_adc : 650;
   uint16_t max_raw = (sensor->calib_code > 2 && sensor->high_adc != 0) ? sensor->high_adc : 1990;
 
   raw = ValueControl(raw, min_raw, max_raw);
 
-  uint16_t apps = (raw - min_raw) * max_pos / (max_raw - min_raw);
+  uint32_t range = max_raw - min_raw;
+  uint32_t apps = ((raw - min_raw) * max_pos + (range / 2)) / range;
 
   if (min_raw > max_raw)
   {
-
-    apps = 100 - apps;
+    apps = 1000 - apps;
   }
   return apps;
 }
 
 uint16_t TF_APPS2(uint8_t bytes, uint32_t raw, Sensor* sensor)
 {
-  uint16_t max_pos = 100; // in percent
+  const uint16_t max_pos = 1000; // in percent
 
   uint16_t min_raw = (sensor->calib_code % 2 == 1 && sensor->low_adc != 0) ? sensor->low_adc : 690;
   uint16_t max_raw = (sensor->calib_code >= 2 && sensor->high_adc != 0) ? sensor->high_adc : 2830;
 
   raw = ValueControl(raw, min_raw, max_raw);
 
-  uint16_t apps = (raw - min_raw) * max_pos / (max_raw - min_raw);
+  uint32_t range = max_raw - min_raw;
+  uint32_t apps = ((raw - min_raw) * max_pos + (range / 2)) / range;
 
   if (min_raw > max_raw)
   {
-
-    apps = 100 - apps;
+    apps = 1000 - apps;
   }
   return apps;
 }
@@ -165,16 +159,9 @@ uint16_t TF_BRK_PRES(uint8_t bytes, uint32_t raw, Sensor* sensor)
   return 0;
 }
 
-uint16_t TF_5V_ASSIGN(uint8_t bytes, uint32_t raw, Sensor* sensor)
+static inline uint16_t GET_5V_LINE(enum Pins pin)
 {
-  (void)bytes;
-
-  uint16_t max_volt = 5000;
-  uint16_t voltage = raw * max_volt / 4095;
-
-  v5_line_mv = voltage;
-
-  return voltage;
+  return sensors[pin].transfer_function(1, sensors[pin].averages, &sensors[pin]);
 }
 
 uint16_t TF_WATER_TEMP(uint8_t bytes, uint32_t raw, Sensor* sensor)
@@ -187,7 +174,9 @@ uint16_t TF_WATER_TEMP(uint8_t bytes, uint32_t raw, Sensor* sensor)
   const float T0 = 298.15f;
   const float R1 = 10000.0f;
 
-  float Vs = (v5_line_mv > 0) ? v5_line_mv : 5000.0f;
+  uint16_t v5_line = GET_5V_LINE(V5_LINE_PIN);
+  // float Vs = (v5_line_mv > 0) ? (float)v5_line_mv : 5000.0f;
+  float Vs = (v5_line > 0) ? (float)v5_line : 5000.0f;
 
   const float Vref = 3300.0f;
 
