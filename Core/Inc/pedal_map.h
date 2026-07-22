b0df9f61f409b03bf5fb8baf14ea51e7c71bd13f
DrinkWaterRightNow <minhnguyen23721@gmail.com>
2026-07-22 17:19:39 +0300

    updated naming

--- Core/Inc/pedal_map.h
+++ Core/Inc/pedal_map.h
@@ -9,7 +9,7 @@
 #define PEDAL_PROFILE_SLOTS 10U
 #define PEDAL_TRANSFER_DONE 0xFFU
 #define PEDAL_PROFILE_SIZE (PEDAL_LUT_SIZE * 2)
-#define PEDAL_TOT_SIZE (PEDAL_PROFILE_SIZE * PEDAL_PROFILE_SLOTS)
+#define PEDAL_PROFILES_TOT_SIZE (PEDAL_PROFILE_SIZE * PEDAL_PROFILE_SLOTS)
 
 #define CAN_CHANGE_PEDAL_PROFILE (0x17)
 #define CAN_ADD_PEDAL_PROFILE (0x18)
@@ -32,13 +32,15 @@ static const uint16_t pedal_profile_soft[PEDAL_LUT_SIZE] = {10,  40,  90,  160, 
 static const uint16_t pedal_profile_stupid[PEDAL_LUT_SIZE] = {20,  60,  120, 200, 320,
                                                               460, 620, 780, 910};
 
+extern const uint16_t* curr_profile;
+void init_pedal_map(void);
 uint16_t pedal_map(uint16_t pedal);
 uint16_t pedal_map_get_percentage();
-extern const uint16_t* curr_profile;
 
 void change_pedal_profile(pedal_profile profile);
 uint8_t add_pedal_profile(void* values, uint8_t slot, uint8_t size);
 void process_pedal_profile_add(CAN_Message msg);
 void process_pedal_profile_change(CAN_Message msg);
+void process_pedal_flash_save(CAN_Message msg);
 
 #endif
